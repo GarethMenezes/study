1ddf471b0dde9c82404d97a6b6397da83ebeb7f5
Draggie <75524491+Draggie306@users.noreply.github.com>
2025-02-10 13:26:43 +0000

    Rename worker.js to index.ts for better naming convention

--- src/backend/worker.js
+++
@@ -1,8 +0,0 @@
-import dbAPI from "./database/dbapi";
-
-// Create an API instance
-const db = new dbAPI(env);
-        
-// Initialise the database if it hasnt been already
-db.initialiseDatabase()
-
