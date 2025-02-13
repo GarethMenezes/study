6f36bc98748b11c3ddfcfcf0a018c2086ede8d1e
Draggie <75524491+Draggie306@users.noreply.github.com>
2025-02-13 14:53:20 +0000

    Use regex

--- src/backend/database/dbapi.ts
+++ src/backend/database/dbapi.ts
@@ -12,37 +12,9 @@ export default class dbAPI {
 
     // Function to check if an email is valid
     async validateEmail(email: string) {
-
-        // Must have "@"
-        if (!email.includes("@"))
-            return false;
-
-        // Ensure there is only one "@"
-        const parts = email.split("@");
-        if (parts.length != 2)
-            return false;
-
-        // Split it into before the "@" and after
-        const [local, domain] = parts;
-
-        // Both sides must exist
-        if (local.length == 0 || domain.length == 0)
-            return false;
-
-
-        // Domain part must have a "."
-        if (!domain.includes("."))
-            return false;
-
-        // Split the domain section by the "."
-        const domainParts = domain.split(".");
-
-        // Both parts must exist
-        if (domainParts[0].length == 0 || domainParts[1].length == 0)
-            return false;
-
-        // If this passes, then the domain "looks" valid
-        return true;
+        const regex_email = new RegExp(/^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/);
+        
+        return regex_email.test(email);
     }
 
 
